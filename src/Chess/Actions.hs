7f1c2c8f1a1274b19cb6ef9dd2c3ffdf2d798f54
Kevin Tai <kevin.r.tai@gmail.com>
2016-12-07 00:22:39 -0600

    Finished first draft of move actions

--- src/Chess/Actions.hs
+++ src/Chess/Actions.hs
@@ -2,31 +2,82 @@ module Chess.Actions where
 import Chess.Base
 import qualified Data.Map as Map
 
+-- Move a piece on the given board from one spot to another, returns Nothing if 
+-- the move is invalid
 movePiece :: Board -> (Int, Int) -> (Int, Int) -> Maybe Board
-movePiece board fromIx = case piece of Piece King c -> moveKing board c fromIx
-                                       Piece Queen c -> moveQueen board c fromIx
-                                       Piece Bishop c -> moveBishop board c fromIx
-                                       Piece Knight c -> moveKnight board c fromIx
-                                       Piece Rook c -> moveRook board c fromIx
-                                       Piece Pawn c -> movePawn board c fromIx
-                                       None -> \(x, y) -> Nothing
-                         where piece = Map.findWithDefault None fromIx board
+movePiece board fromIx toIx
+  | not (inRange fromIx) || not (inRange toIx)         = Nothing
+  | (target == None)                                   = Nothing
+  | (taken == None) || (color target) /= (color taken) = case target of Piece King c -> moveKing board fromIx toIx
+                                                                        Piece Queen c -> moveQueen board fromIx toIx
+                                                                        Piece Bishop c -> moveBishop board fromIx toIx
+                                                                        Piece Knight c -> moveKnight board fromIx toIx
+                                                                        Piece Rook c -> moveRook board fromIx toIx
+                                                                        Piece Pawn c -> movePawn board fromIx toIx
+                                                                        None -> Nothing
+  | otherwise = Nothing
+  where target = Map.findWithDefault None fromIx board
+        taken = Map.findWithDefault None toIx board
 
-moveKing :: Board -> Color -> (Int, Int) -> (Int, Int) -> Maybe Board
-moveKing _ _ _ _ = Nothing
 
-moveQueen :: Board -> Color -> (Int, Int) -> (Int, Int) -> Maybe Board
-moveQueen _ _ _ _ = Nothing
+-- Piece specific move functions that moves the piece at fromIx to toIx as though it is a 
+-- King, Queen, Bishop, etc. Return Nothing if the move is invalid
+moveKing :: Board -> (Int, Int) -> (Int, Int) -> Maybe Board
+moveKing board fromIx toIx
+  | fromIx /= toIx && (abs (x0 - x1)) <= 1 && (abs (y0 - y1)) <= 1 = move board fromIx toIx
+  | otherwise                                                      = Nothing
+  where (x0, y0) = fromIx
+        (x1, y1) = toIx
 
-moveBishop :: Board -> Color -> (Int, Int) -> (Int, Int) -> Maybe Board
-moveBishop _ _ _ _ = Nothing
+moveQueen :: Board -> (Int, Int) -> (Int, Int) -> Maybe Board
+moveQueen board fromIx toIx
+  | fromIx /= toIx && (abs (x0 - x1) == abs (y0 - y1) || x0 == x1 || y0 == y1) = move board fromIx toIx
+  | otherwise                                                                  = Nothing
+  where (x0, y0) = fromIx
+        (x1, y1) = toIx
 
-moveKnight :: Board -> Color -> (Int, Int) -> (Int, Int) -> Maybe Board
-moveKnight _ _ _ _ = Nothing
+moveBishop :: Board -> (Int, Int) -> (Int, Int) -> Maybe Board
+moveBishop board fromIx toIx
+  | fromIx /= toIx && (abs (x0 - x1) == abs (y0 - y1)) = move board fromIx toIx
+  | otherwise                                          = Nothing 
+  where (x0, y0) = fromIx
+        (x1, y1) = toIx
 
-moveRook :: Board -> Color -> (Int, Int) -> (Int, Int) -> Maybe Board
-moveRook _ _ _ _= Nothing
+moveKnight :: Board -> (Int, Int) -> (Int, Int) -> Maybe Board
+moveKnight board fromIx toIx
+  | fromIx /= toIx && ((dx == 1 && dy == 2) || (dx == 2 && dy == 1)) = move board fromIx toIx
+  | otherwise                                                        = Nothing
+  where (x0, y0) = fromIx
+        (x1, y1) = toIx
+        dx       = abs (x0 - x1)
+        dy       = abs (y0 - y1)
 
-movePawn :: Board -> Color -> (Int, Int) -> (Int, Int) -> Maybe Board
-movePawn _ _ _ _ = Nothing
+moveRook :: Board -> (Int, Int) -> (Int, Int) -> Maybe Board
+moveRook board fromIx toIx
+  | fromIx /= toIx && (x0 == x1 || y0 == y1) = move board fromIx toIx
+  | otherwise                                = Nothing
+  where (x0, y0) = fromIx
+        (x1, y1) = toIx
 
+-- TODO add en passant and diagonals
+movePawn :: Board -> (Int, Int) -> (Int, Int) -> Maybe Board
+movePawn board fromIx toIx
+  | fromIx /= toIx && (y1 - y0) == direction = move board fromIx toIx
+  | otherwise                                = Nothing
+  where target    = Map.findWithDefault None fromIx board
+        direction = if (color target == White)
+                      then 1 else -1
+        (x0, y0)  = fromIx
+        (x1, y1)  = toIx
+
+move :: Board -> (Int, Int) -> (Int, Int) -> Maybe Board
+move board fromIx toIx = Just result
+  where piece      = Map.findWithDefault None fromIx board
+        removeFrom = Map.delete fromIx board
+        removeTo   = Map.delete toIx removeFrom
+        result     = Map.insert toIx piece removeTo
+
+
+-- Helper functions
+inRange :: (Int, Int) -> Bool
+inRange (x, y) = x >= 1 && x <= 8 && y >= 1 && y <= 8
